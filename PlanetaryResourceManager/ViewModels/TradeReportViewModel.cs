f72baf2876d535db65600f50dbaa7d9c92cef0e8
tuvoksg1 <tuvoksg1@hotmail.com>
2015-05-31 07:17:53 +0100

    Relinked files on new PC
    Nothing changed

---
+++ PlanetaryResourceManager/ViewModels/TradeReportViewModel.cs
@@ -0,0 +1,32 @@
+﻿using PlanetaryResourceManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetaryResourceManager.ViewModels
+{
+    class TradeReportViewModel : BaseViewModel
+    {
+        private TradeItem _currentItem;
+
+        public TradeReportViewModel(List<TradeItem> tradeItems)
+        {
+            Commodities = new ObservableCollection<TradeItem>(tradeItems);
+        }
+
+        public  ObservableCollection<TradeItem> Commodities { get; set; }
+
+        public TradeItem CurrentItem
+        {
+            get { return _currentItem; }
+            set
+            {
+                _currentItem = value;
+                RaisePropertyChanged("CurrentItem");
+            }
+        }
+    }
+}
