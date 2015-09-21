c126c64382d28662f4560779f2b6f3f7691dc4ea
tuvoksg1 <tuvoksg1@hotmail.com>
2015-09-21 16:24:46 +0100

    Added SignalR support on API project

---
+++ PlanetaryResourceManager.Api/SignalR/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(startupType:(typeof(PlanetaryResourceManager.Api.SignalR.Startup)))]
+namespace PlanetaryResourceManager.Api.SignalR
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
