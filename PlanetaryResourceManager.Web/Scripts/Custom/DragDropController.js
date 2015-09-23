177628d5298cdd5c75b102502a6855674e4d090d
tuvoksg1 <tuvoksg1@hotmail.com>
2015-09-23 12:31:55 +0100

    Updated drag & drop functionality to preserve original items

--- PlanetaryResourceManager.Web/Scripts/Custom/DragDropController.js
+++ PlanetaryResourceManager.Web/Scripts/Custom/DragDropController.js
@@ -20,8 +20,28 @@
 
             $(".treeViewPanel").kendoTreeView({
                 dragAndDrop: true,
+                drop: onItemDrop
             });
         };
+
+        function onItemDrop(e) {
+            e.preventDefault();
+
+            var sourceItem = this.dataItem(e.sourceNode).toJSON();
+            var destinationNode = $(e.destinationNode);
+            var targetTree = destinationNode.closest("[data-role='treeview']").data("kendoTreeView");
+
+            if (e.dropPosition == "before") {
+                targetTree.insertBefore(sourceItem, destinationNode);
+            } else if (e.dropPosition == "after") {
+                targetTree.insertAfter(sourceItem, destinationNode);
+            } else {
+                targetTree.append(sourceItem, destinationNode);
+            }
+            $scope.$apply(function () {
+                $scope.updateText = "You compeleted a drag and drop!";
+            });
+        }
 
         var onError = function (reason) {
             $scope.error = "Could not fetch the data";
@@ -30,10 +50,15 @@
         var loadLevel = function () {
             apiService.getProductsForLevel($scope.currentLevel)
                 .then(onProductLoad, onError);
-        }
+        };
 
-        $scope.currentLevel = "Raw Materials";
-        loadLevel();
+        var activate = function () {
+            $scope.currentLevel = "Refined Materials";
+            $scope.updateText = "Select an item to drag";
+            loadLevel();
+        };
+
+        activate();
     };
 
     app.controller("DragDropController", dragDropController);
