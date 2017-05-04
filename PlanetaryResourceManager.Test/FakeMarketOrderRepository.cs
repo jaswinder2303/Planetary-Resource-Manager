37303f64988f61b62407b76493721b906560afaa
tuvoksg1 <tuvoksg1@hotmail.com>
2017-05-04 19:48:47 +0100

    Removed packages from SCM

---
+++ PlanetaryResourceManager.Test/FakeMarketOrderRepository.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using PlanetaryResourceManager.Core.Models;
+
+namespace PlanetaryResourceManager.Test
+{
+    static class FakeMarketOrderRepository
+    {
+        internal static void Initialize()
+        {
+            BuyOrders = new List<MarketOrder>
+            {
+                MakeOrder(2.2, 50, MarketDataResponse.HighSec),
+                MakeOrder(2.2, 50, MarketDataResponse.HighSec),
+                MakeOrder(2.2, 50, MarketDataResponse.HighSec),
+                MakeOrder(2.2, 50, MarketDataResponse.HighSec),
+                MakeOrder(2.2, 50, MarketDataResponse.HighSec),
+                MakeOrder(2.2, 50, MarketDataResponse.HighSec)
+            };
+
+            SellOrders = new List<MarketOrder>
+            {
+                MakeOrder(2.2, 50, MarketDataResponse.HighSec),
+                MakeOrder(2.2, 50, MarketDataResponse.HighSec),
+                MakeOrder(2.2, 50, MarketDataResponse.HighSec),
+                MakeOrder(2.2, 50, MarketDataResponse.HighSec),
+                MakeOrder(2.2, 50, MarketDataResponse.HighSec),
+                MakeOrder(2.2, 50, MarketDataResponse.HighSec)
+            };
+        }
+
+        internal static List<MarketOrder> BuyOrders { get; private set; }
+
+        internal static List<MarketOrder> SellOrders { get; private set; }
+
+        private static MarketOrder MakeOrder(double price, int quantity, double security)
+        {
+            return new MarketOrder
+            {
+                Price = price,
+                Quantity = quantity,
+                Security = security + 0.1
+            };
+        }
+    }
+}
---
+++ PlanetaryResourceManager.Test/MarketDataResponseTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using PlanetaryResourceManager.Core.Models;
+
+namespace PlanetaryResourceManager.Test
+{
+    [TestFixture]
+    public class MarketDataResponseTest
+    {
+        private MarketDataResponse _dataResponse;
+
+        [SetUp]
+        public void Initialize()
+        {
+            FakeMarketOrderRepository.Initialize();
+
+            _dataResponse = new MarketDataResponse
+            {
+                BuyOrders = FakeMarketOrderRepository.BuyOrders,
+                SellOrders = FakeMarketOrderRepository.SellOrders
+            };
+
+            MarketDataResponse.ResequenceOrders(_dataResponse);
+        }
+
+        [Test]
+        public void GetDefaultLowestOrder()
+        {
+            _dataResponse.SecurityLevel = MarketDataResponse.HighSec;
+            var order = _dataResponse.LowestSellOrder(40);
+
+            Assert.AreEqual(2.2, order.Price, "The expected order price was not returned");
+            Assert.AreEqual(50, order.Quantity, "The expected order quantity was not returned");
+        }
+
+        [Test]
+        public void GetDefaultHighestOrder()
+        {
+            _dataResponse.SecurityLevel = MarketDataResponse.HighSec;
+            var order = _dataResponse.HighestBuyOrder;
+
+            Assert.AreEqual(2.2, order.Price, "The expected order price was not returned");
+            Assert.AreEqual(50, order.Quantity, "The expected order quantity was not returned");
+        }
+    }
+}
